cdfe969845d4546ebcabbf5bd181a771e4830c97
ABAS Token <120234697+AbasToken@users.noreply.github.com>
2023-04-01 17:39:36 -0500

    Update ABAS.sol

--- solidity-contracts/ABAS.sol
+++ solidity-contracts/ABAS.sol
@@ -1,5 +1,6 @@
 /**
 Testnet added _MINIMUM_TARGET = _MAXIMUM_TARGET.div(100); for 100 difficulty maximum on testnet, doubles every era
+change howMuchETH equation to suit mainnet better. simple 4 stages for testing purposes
  *Submitted for verification at Etherscan.io on 2022-12-20
 */
 
